8ab58e14cb0d2b2258fc9bf92616b5dc71184a7b
catapecas <zoraya.giraldo@campusucc.edu.co>
2025-02-07 01:01:39 +0000

    se agrego la palabra JAVA

--- clase1/ejecutar.java
+++ clase1/ejecutar.java
@@ -1,7 +1,7 @@
 public class ejecutar {
     public static void main(String[] args) {
-        System.out.println("HOLA MUNDO!!!");
-        
+        System.out.println("HOLA MUNDO!!! JAVA");
+
     }
 
     
