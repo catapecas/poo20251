b4be4ae74d16c610ce65ec126b276a8ba5bb3b54
catapecas <zoraya.giraldo@campusucc.edu.co>
2025-02-14 01:22:29 +0000

    ejercicios clase 2

---
+++ clase 2/Bucles.java
@@ -0,0 +1,27 @@
+public class Bucles {
+    public static void main(String[] args) {
+        // for (inicio;condicion de parada ; incremento o decremento)
+        
+        for (int i = 0; i<=5; i++){
+            System.out.println("Numero de Iteracion: " + i);
+
+        }
+        /* estructura del while
+        *
+        *inicio;
+        * while(condicion){
+        *-----------
+        *}
+        *incremento o decremento
+        */
+        int j = 0;
+        while (j <=5) {
+            System.out.println("Numero de Iteracion: " + j);
+            j++;
+
+            
+        }
+    }
+    
+}
+
---
+++ clase 2/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    // Metodo main
+
+    public static void main(String[] args) {
+
+        //Tipos de datos
+        //Enteros int, long 
+        int edad= 25;
+        // Reales double, float
+        double altura =1.75;
+        //cadenas
+        String nombre ="juan";
+        // logicos
+        boolean esEstudiante = true;// false
+        System.out.println("Nombre: "+ nombre + ",Edad: " + edad + ", Altura: " + altura 
+        + ", Estudiante: " + esEstudiante );
+
+
+        
+    }
+}
