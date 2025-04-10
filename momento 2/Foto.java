497ec085619ce2ab18bc9b7615f271c5f424c038
catapecas <zoraya.giraldo@campusucc.edu.co>
2025-04-10 04:07:38 +0000

    void foto

--- momento 2/Foto.java
+++ momento 2/Foto.java
@@ -2,9 +2,45 @@ public class Foto {
 
     //Atributos
     private String fichero;
+    private String fotoColor;
+    private String fotoBlancoNegro;
+    private int numeroCopias;
 
-    // Metodos
-    public void print(){
-        
+    // constructor 
+    public Foto(String fichero, String fotoColor, String fotoBlancoNegro, int numeroCopias) {
+        this.fichero = fichero;
+        this.fotoColor = fotoColor;
+        this.fotoBlancoNegro = fotoBlancoNegro;
+        this.numeroCopias = numeroCopias;
     }
+   
+      // Metodos
+    public void setFichero(String fichero) {
+        this.fichero = fichero;
+    }
+    public String getFichero() {
+        return fichero;
+    }
+    public void setFotoColor(String fotoColor) {
+        this.fotoColor = fotoColor;
+    }
+    public String getFotoColor() {
+        return fotoColor;
+    }
+    public void setFotoBlancoNegro(String fotoBlancoNegro) {
+        this.fotoBlancoNegro = fotoBlancoNegro;
+    }
+    public String getFotoBlancoNegro() {
+        return fotoBlancoNegro;
+    }
+    public void setNumeroCopias(int numeroCopias) {
+        this.numeroCopias = numeroCopias;
+    }
+    public int getNuemeoCopias() {
+        return numeroCopias;
+    }
+    public String toString() {
+        return "Foto{" + "fichero=" + fichero + ", fotoColor=" + fotoColor + ", fotoBlancoNegro=" + fotoBlancoNegro + ", numeroCopias=" + numeroCopias + '}';
+    }
+
 }
