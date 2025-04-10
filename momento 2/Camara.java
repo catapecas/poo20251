db78833d20d901e7edb34a9d021d728a8b472272
catapecas <zoraya.giraldo@campusucc.edu.co>
2025-04-10 02:42:00 +0000

    constructor camara

--- momento 2/Camara.java
+++ momento 2/Camara.java
@@ -3,8 +3,13 @@ public class Camara extends Producto {
     //Atributos
     private String marca;
     private String modelo;
-    private
-
-    // Metodos
-    
+    private double codigo;    
 }
+
+  // constructor
+  public Camara (String marca, String modelo, double codigo){
+    super(getNumero(), marca, modelo, codigo, codigo)
+    this.marca = marca;
+    this.modelo = modelo;
+    this.codigo = codigo;
+  }
